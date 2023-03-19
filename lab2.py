09b6511507a7e9a643fc9216c156d1e75b842a5a
Artemii <102295911+TyomaAs@users.noreply.github.com>
2023-03-19 19:50:27 +0200

    add functions (unworker)

--- lab2.py
+++ lab2.py
@@ -2,6 +2,13 @@ import math
 import string
 import fileinput
 
+def showInfo(filePerson, filePay, id):
+    person.id = id
+
+
+def showID(filePerson, filePay, id):
+    for i in filePerson:
+
 
 class person:
     id = 0
@@ -25,19 +32,6 @@ while no_exit_program:
     print("4) Show persons by the most popular profession:")
     print("5) Exit program:")
     choice = int(input("Enter the choice:\t"))
-    switch(choice):
-        case '1':
-            a = 2
-
-
-''' Завдання
-Файл "Спiвробiтники" мiстить наступну iнформацiю про спiвробiтникiв: iндентифiкацiйний код, прiзвище та iнiцiали, вид освiти, фах, рiк народження. 
-
-Файл "Зарплата" мiстить iндентифiкацiйний код, зарплату за перше пiврiччя, зарплату за друге пiврiччя. 
-
-Розв’язати наступнi задачi:
-a) Вивести прiзвища спiвробiтникiв з заданим видом освiти.
-b) Вивести кiлькостi спiвробiтникiв, за кожним фахом.
-c) Вивести зарплати спiвробiтникiв з найпопулярнiшим фахом.
-
-Для обробки iнформацiї про спiвробiтника використовувати власний клас.'''
+    if choice == 1:
+        id = int(input("Enter the ID: "))
+        showID(id)
